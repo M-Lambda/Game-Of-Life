19778a236a359f46d7ea0b5e8c71ab31f812fd20
MaryamMosstoufi <maryam@zooxoos.com>
2020-09-22 15:06:17 -0400

    Created About and Rules Page

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Game of Life</h1>
+    about page
+  </div>
+</template>
---
+++ pages/rules.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Game of Life</h1>
+    rules page
+  </div>
+</template>
