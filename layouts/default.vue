cfe7999867c78e9e8d28870681e71e4866b1c46e
MaryamMosstoufi <maryam@zooxoos.com>
2020-09-22 15:15:44 -0400

    Initial Layout Design

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,46 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+  <a-layout id="components-layout-demo-custom-trigger">
+    <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
+      <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
+        <a-menu-item key="1">
+          <nuxt-link to='/'>
+            <a-icon type="border-outer" />
+            <span>Game of Life</span>
+          </nuxt-link>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="setting" />
+          <span>Settings</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <nuxt-link to='/rules'>
+            <a-icon type="profile" />
+            <span>Rules</span>
+          </nuxt-link>
+        </a-menu-item>
+        <a-menu-item key="4" href='/about'>
+          <nuxt-link to='/about'>
+            <a-icon type="user" />
+            <span> About </span>
+          </nuxt-link>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-content
+        :style="{ margin: '24px', padding: '24px', background: '#fff', minHeight: '280px' }"
+      >
+        <Nuxt />
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
-
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+};
+</script>
